fe9704ad7e202562d108ebbc850e4c9784b473ca
igorcoding <igorcoding@gmail.com>
2016-08-23 13:10:32 +0300

    moved raft to separate repo

---
+++ queuelight.lua
@@ -0,0 +1,161 @@
+local fiber = require('fiber')
+local log = require('log')
+local msgpack = require('msgpack')
+local obj = require('obj')
+local uuid = require('uuid')
+
+local errorcode = require('errorcode')
+local util = require('util')
+
+local STATUS = {
+	R = 'R',
+	T = 'T',
+	B = 'B',
+}
+local DEFAULT_TIMEOUT = 60
+
+local queue = obj.class({}, 'queuelight')
+
+queue.STATUS = STATUS
+queue.E = errorcode{}
+queue.E:register({
+	{ code = 101, name = 'TASK_NOT_TAKEN', msg = "Task %s not taken by anybody" },
+	{ code = 102, name = 'TASK_TAKEN_NOT_BY_YOU', msg = "Task %s taken by %s, not you (%s)" },
+	{ code = 103, name = 'TASK_NOT_FOUND', msg = "Task %s not found" },
+})
+
+local _wait = {}
+
+function queue:_init(opts)
+	util.args_required(opts, {'space', 'index_queue', 'index_primary', 'f_id', 'f_status'})
+	self._consumers = {}
+	self._taken = {}
+	self._wseq = 1
+
+	self.space = opts.space
+	self.index_queue = opts.index_queue
+	self.index_primary = opts.index_primary
+	self.f_id = opts.f_id
+	self.f_status = opts.f_status
+
+	assert(box.space[self.space], "Unknown space " .. self.space)
+	assert(box.space[self.space].index[self.index_queue], "Unknown queue index " .. self.space .. "." .. self.index_queue)
+	assert(box.space[self.space].index[self.index_primary], "Unknown primary index " .. self.space .. "." .. self.index_primary)
+end
+
+
+function queue:wakeup(t)
+	if t[self.f_status] ~= STATUS.R then return end
+	for _, v in pairs(_wait) do
+		v:put(t, 0)
+		return
+	end
+	-- print("No waits")
+end
+
+function queue:check_owner(k)
+	if not self._taken[k] then
+		-- queue.E:raise(queue.E.TASK_NOT_TAKEN, k)
+	end
+	if self._taken[k] ~= nil and self._taken[k] ~= box.session.id() then
+		queue.E:raise(queue.E.TASK_TAKEN_NOT_BY_YOU, k, self._taken[k], box.session.id())
+	end
+	return true
+end
+
+function queue:set_status(task_id, status)
+	return box.space[self.space]:update({task_id}, {{'=', self.f_status, status}})
+end
+
+function queue:release(task_id,opt)
+	self:check_owner(task_id)
+	opt = opt or {}
+
+	t = self:set_status(task_id, STATUS.R)
+	if t and not opt['no_wakeup'] then
+		self:wakeup(t)
+	end
+
+	local sid = self._taken[task_id]
+	self._taken[task_id] = nil
+	if self._consumers[sid] then
+		self._consumers[sid][task_id] = nil
+	end
+	return t
+end
+
+function queue:done(task_id,opt)
+	self:check_owner(task_id)
+	opt = opt or {}
+
+	t = self:set_status(task_id, STATUS.R)
+
+	local sid = self._taken[task_id]
+	self._taken[task_id] = nil
+	if self._consumers[sid] then
+		self._consumers[sid][task_id] = nil
+	end
+	return t
+end
+
+function queue:bury(task_id,opt)
+	self:check_owner(task_id)
+	opt = opt or {}
+
+	t = self:set_status(task_id, STATUS.B)
+
+	local sid = self._taken[task_id]
+	self._taken[task_id] = nil
+	if self._consumers[sid] then
+		self._consumers[sid][task_id] = nil
+	end
+	t = box.space[self.space]:delete({task_id})
+	return t
+end
+
+function queue:taken(task)
+	local sid = box.session.id()
+	if self._consumers[sid] == nil then
+		self._consumers[sid] = {}
+	end
+	local k = task[self.f_id]
+	local t = self:set_status(k, STATUS.T)
+	
+	self._consumers[sid][k] = { util.time(), box.session.peer(sid), t }
+	self._taken[k] = sid
+	return t
+end
+
+function queue:wait(wait_time)
+	local wseq = self._wseq
+	self._wseq = wseq + 1
+
+	local ch = fiber.channel(1)
+	_wait[wseq] = ch
+	local t = ch:get(wait_time)
+	_wait[wseq] = nil
+	return t
+end
+
+function queue:on_disconnect(sid)
+	local peer = '<PEERNAME>' -- box.session.peer(sid)
+	local now = util.time()
+
+	if self._consumers[sid] ~= nil then
+		local consumers = self._consumers[sid]
+		for k,rec in pairs(consumers) do
+			time, peer, task = unpack(rec)
+
+			local v = box.space[self.space].index[self.index_primary]:get({k})
+
+			if v ~= nil and v[self.f_status] == STATUS.T then
+				log.info("[ERR] Requeue: %s back to %s by disconnect from %d/%s; taken=%0.6fs", k, STATUS.R, sid, peer, tonumber(now - time))
+				v = self:release(v[self.f_id])
+			end
+		end
+		self._consumers[sid] = nil
+	end
+end
+
+
+return queue
---
+++ util.lua
@@ -0,0 +1,93 @@
+local fiber = require('fiber')
+local msgpack = require('msgpack')
+local uuid = require('uuid')
+
+local errorcode = require('errorcode')
+
+local util = {}
+util.E = errorcode{}
+util.E:register({
+	{ code = 201, name = 'ARG_MISSING', msg = "Field %s is missing" },
+	{ code = 202, name = 'ARGS_MISSING', msg = "Arguments are missing" },
+	{ code = 203, name = 'ARGS_MISSING_NAME', msg = "Argument '%s' is missing" },
+})
+
+function util.time()
+	return fiber.time()
+end
+
+function util.itime()
+	return 0ULL + fiber.time()
+end
+
+function util.print_table(table)
+	for key, value in pairs(table) do
+		print(key, value)
+	end
+end
+
+function util.get_or_null(table, key)
+	if table[key] == nil then
+		return msgpack.NULL
+	else
+		return table[key]
+	end
+end
+
+function util.get_or(table, key, fallback)
+	if table[key] == nil then
+		return fallback
+	else
+		return table[key]
+	end
+end
+
+function util._arg_required(args, key)
+	if args[key] == nil then
+		util.E:raise(util.E.ARG_MISSING, key)
+	end
+	return args[key]
+end
+
+function util.arg_required(args, key)
+	if args == nil then
+		util.E:raise(util.E.ARGS_MISSING)
+	end
+	util._arg_required(args, key)
+end
+
+function util.args_required(args, keys, args_name)
+	if args == nil then
+		if args_name == nil then
+			util.E:raise(util.E.ARGS_MISSING)
+		else
+			util.E:raise(util.E.ARGS_MISSING_NAME, args_name)
+		end
+	end
+	for k, v in pairs(keys) do
+		util._arg_required(args, v)
+	end
+end
+
+function util.iter(index, ...)
+	local f,s,var = index:pairs(...)
+	local iterator = {
+		f = f,
+		s = s,
+		var = var,
+	}
+
+	local mt = {
+		__call = function(t)
+			return t.f(t.s, t.var)
+		end
+	}
+
+	return setmetatable(iterator, mt)
+end
+
+function util.uuid()
+	return uuid():str()
+end
+
+return util
